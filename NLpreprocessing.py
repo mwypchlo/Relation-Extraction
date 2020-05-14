1326cff4861caee924d7288cf583094df94169e0
michalw <michal.wypchlo@vp.pl>
2020-05-15 00:01:14 +0200

    tagging words

--- NLpreprocessing.py
+++ NLpreprocessing.py
@@ -8,6 +8,7 @@ def get_sentences(text):
 def get_words(text):
     data = nltk.word_tokenize(text)
     return data
+
 def remove_stopwords(text):
     filtered_words=[]
     words=get_words(text)
@@ -16,6 +17,11 @@ def remove_stopwords(text):
             filtered_words.append(w)
     return filtered_words
 
+def tagging_data(text):##explanations of tags in tags_explanation file
+    for t in text:
+        words=remove_stopwords(text)
+        tagged= nltk.pos_tag(words)
+    return tagged
 
 ##test
 text = "Rick Grimes is a sheriff of King County"
@@ -23,3 +29,4 @@ text = "Rick Grimes is a sheriff of King County"
 print(get_sentences(text))
 print(get_words(text))
 print(remove_stopwords(text))
+print(tagging_data(text))
