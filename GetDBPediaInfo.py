ac64bf1d5a99c877d58421ecf5976b5727f9dd4b
AdamskiR <rafal.adamsk@gmail.com>
2020-06-15 18:43:34 +0200

    Added knowlegeBase.py + SPARQL querry change

--- GetDBPediaInfo.py
+++ GetDBPediaInfo.py
@@ -8,9 +8,6 @@ def printResult(results):
 def filterAnswear(dict):
     for x in dict["results"]["bindings"]:
         temp = str(x["type"]["value"])
-        # temp = temp[temp.rfind('/') + 1:]
-        # print(temp)
-        # print(temp[temp.rfind('/')+1:])
         x["type"]["value"] = temp[temp.rfind('/') + 1:]
 
     return dict
@@ -20,10 +17,13 @@ def getTypes(object):
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
     querry = """
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+
     SELECT ?type 
     WHERE { <http://dbpedia.org/resource/"""
     querry += object
-    querry += "> rdf:type ?type}"
+    querry += """> rdf:type ?type 
+    FILTER ( strstarts(str(?type), "http://dbpedia.org/ontology/" ) ) }"""
 
     sparql.setQuery(querry)
     sparql.setReturnFormat(JSON)
--- main.py
+++ main.py
@@ -4,16 +4,25 @@ import re
 import GetDBPediaInfo
 import NLpreprocessing
 
-getinfo = GetDBPediaInfo.getTypes('Bill_Gates')
+# getinfo = GetDBPediaInfo.getTypes('Bill_Gates')
+
+# if __name__ == '__main__':
+#     text = raw_input("Write the sentence: ")
+#     nl_data = NLpreprocessing.get_continuous_chunks(text)
+#     print(nl_data)
+#     for p in nl_data:
+#         s = p.replace(" ", "_")
+#         print(s)
+#         GetDBPediaInfo.printResult(GetDBPediaInfo.getTypes(s))
+#         processedInfo = GetDBPediaInfo.filterAnswear(GetDBPediaInfo.getTypes(s))
+#         GetDBPediaInfo.printResult(processedInfo)
+#         print("------------------------------------------------------------------------------------")
 
 if __name__ == '__main__':
-    text=raw_input("Write the sentence: ")
-    nl_data=NLpreprocessing.get_continuous_chunks(text)
-    print(nl_data)
-    for p in nl_data:
-        s= p.replace(" ", "_")
-        print(s)
-        GetDBPediaInfo.printResult(GetDBPediaInfo.getTypes(s))
-        processedInfo = GetDBPediaInfo.filterAnswear(GetDBPediaInfo.getTypes(s))
-        GetDBPediaInfo.printResult(processedInfo)
-        print("------------------------------------------------------------------------------------")
+    getinfo = GetDBPediaInfo.getTypes('Bill_Gates')
+    getinfo2 = GetDBPediaInfo.getTypes('Microsoft')
+    processedInfo1 = GetDBPediaInfo.filterAnswear(getinfo)
+    processedInfo2 = GetDBPediaInfo.filterAnswear(getinfo2)
+    GetDBPediaInfo.printResult(processedInfo1)
+    print("-------------------------------------------------")
+    GetDBPediaInfo.printResult(processedInfo2)
